892b88f0a18130e62a516a8566221ff9d531039a
kyuhyunp <kyuhyunp@umich.edu>
2023-09-05 16:26:39 -0700

    Moved model.py and used agent_chain to add memory to the google search agent

--- Backend/model.py
+++ Backend/model.py
@@ -1,5 +1,6 @@
 from constants import openapi_key
 from constants import serpapi_key
+from langchain import LLMChain
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.document_loaders import PyPDFLoader
@@ -8,7 +9,7 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import SerpAPIWrapper
-from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.agents import AgentType, initialize_agent, load_tools, AgentExecutor, ZeroShotAgent
 
 import os, sys
 
@@ -18,7 +19,7 @@ sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 os.environ['OPENAI_API_KEY'] = openapi_key
 os.environ['SERPAPI_API_KEY'] = serpapi_key
 #llm = OpenAI(temperature=0.7)
-loader = PyPDFLoader("data/email.pdf")
+loader = PyPDFLoader("Data/email.pdf")
 documents = loader.load_and_split()
 
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
@@ -50,9 +51,28 @@ print(result["answer"])
 
 llm = OpenAI(temperature=0)
 tools = load_tools(["serpapi"], llm=llm)
-agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION)
 
-print(agent.run("What is the URL address of the company Unisys?"))
+## Use agent chain for memory
+prefix = """Have a conversation with a human, answering the following questions as best you can. You have access to the following tools:"""
+suffix = """Begin!"
+
+{chat_history}
+Question: {input}
+{agent_scratchpad}"""
+
+prompt = ZeroShotAgent.create_prompt(
+    tools,
+    prefix=prefix,
+    suffix=suffix,
+    input_variables=["input", "chat_history", "agent_scratchpad"],
+)
+# Research into different agents
+llm_chain = LLMChain(llm=llm, prompt=prompt)
+agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=True)
+agent_chain = AgentExecutor.from_agent_and_tools(
+    agent=agent, tools=tools, verbose=True, memory=memory
+)
+print(agent_chain.run("What is the URL address of the company?"))
 
 
 
